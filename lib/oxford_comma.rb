a635025f533b86fb33e050162258ebd10d0079d3
Thompson Plyler <nealok.g@gmail.com>
2018-11-04 00:39:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-if array.length=1
-  array
-else
-last = array.pop
-string = array.join(",")
-string + ", and" + ", #{last}"
-end
+  if array.length=1
+    array
+  else
+    last = array.pop
+    string = array.join(",")
+    string + ", and" + ", #{last}"
+  end
 end
